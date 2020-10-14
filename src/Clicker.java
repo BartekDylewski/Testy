5b84de8778fdf800b7659eca07a0a95127d407a2
EloBenccc <71230307+EloBenccc@users.noreply.github.com>
2020-10-14 21:57:20 +0200

    v1.3.1

--- src/Clicker.java
+++ src/Clicker.java
@@ -1,64 +1,94 @@
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Clicker implements ActionListener{
+class Clicker implements ActionListener{
 
     private double points;
     private double multiplier;
+    private double increaseMultiplierPrize;
     private JLabel label;
     private JFrame frame;
     private JPanel panel;
     private JButton clicker;
-    private JButton zwiekszMnoznik;
+    private JButton increaseMultiplier;
 
     public Clicker(){
 
         multiplier = 1;
+        increaseMultiplierPrize = 10;
 
         frame = new JFrame();
-        frame.setSize(300, 200);
+        frame.setSize(310, 170);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
 
         panel = new JPanel();
         panel.setLayout(null);
+        panel.setBackground(Color.darkGray);
 
         clicker = new JButton("Click me!");
+        clicker.setBackground(Color.lightGray); // Ustawia kolor tła przycisku
+        clicker.setForeground(Color.darkGray); // Ustawia kolor czcionki
+        clicker.setFocusPainted(false); // Usuwa kwadraty po najechaniu kursorem na button
         clicker.setBounds(20, 20, 250, 50);
         clicker.addActionListener(this);
 
-        zwiekszMnoznik = new JButton("Increase Multiplier by 1 for 10 points");
-        zwiekszMnoznik.setBounds(20, 70, 250 ,25);
-        zwiekszMnoznik.addActionListener(new ActionListener() {
+        increaseMultiplier = new JButton("Increase Multiplier by 0.5 for " + increaseMultiplierPrize + " points");
+        increaseMultiplier.setBackground(Color.lightGray); // Ustawia kolor tła przycisku
+        increaseMultiplier.setForeground(Color.darkGray); // Ustawia kolor czcionki
+        increaseMultiplier.setFocusPainted(false); // Usuwa kwadraty po najechaniu kursorem na button
+        increaseMultiplier.setBounds(20, 75, 250 ,25);
+        increaseMultiplier.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(points >= 10){
-                    multiplier++;
-                    points = points - 10;
-                    label.setText("Points: " + points);
+                if(points >= increaseMultiplierPrize){
+                    multiplier = multiplier + 0.5; // Dodaje 0.5 do mnożnika
+                    points = points - increaseMultiplierPrize; // Odejmuje cenę zwiększenia mnożnika
+                    increaseMultiplierPrize *= 2; // Zwiększa cenę
+                    label.setText("Points: " + points); // Wyświetla punkty
+                    increaseMultiplier.setText("Increase Multiplier by 0.5 for " + increaseMultiplierPrize + " points");
                 }
-
             }
         });
 
-        label = new JLabel("Points; " + points);
-        label.setBounds(20, 90, 250, 50);
+        label = new JLabel("Points: " + points);  // Wyświetla punkty
+        label.setBounds(20, 85, 250, 50);
+        label.setForeground(Color.lightGray); // Zmienia kolor czcionki
 
-        frame.add(zwiekszMnoznik);
+        frame.add(increaseMultiplier);
         frame.add(label);
         frame.add(clicker);
         frame.add(panel);
         frame.setVisible(true);
+
+        Timer t = new Timer( );
+        t.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                points = points + 1;
+                label.setText("Points: " + points); // Wyświetla punkty
+            }
+        }, 1000,1000);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        points = points + (multiplier); // Dodaje do punktów 1*mnożnik
+        label.setText("Points: " + points); // Wyświetla punkty
     }
 
     public static void main(String[] args) {
         new Clicker();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        points = points + (1 * multiplier);
-        label.setText("Points: " + points);
-    }
 }
+
+
+
+
+
